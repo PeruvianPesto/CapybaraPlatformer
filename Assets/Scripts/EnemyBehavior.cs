c78e6272abcd32dcb703a3e0368a68f687e7067e
PeruvianPesto <stefanopse04@gmail.com>
2024-03-30 18:49:45 -0700

    Enemy Walk Cycles added

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed;
+    private Transform currentPoint;
+    [SerializeField] private Rigidbody2D rb;
+    void Start()
+    {
+        currentPoint = pointB.transform;
+    }
+
+    
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;  
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);  
+    }
+}
